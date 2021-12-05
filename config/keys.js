57e253a09e50d9583c40efc419fa4f234581699d
jacky <jc6825@stern.nyu.edu>
2021-12-05 14:43:51 -0500

    first commit

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://jackychen6825:JCNzUgf9crpKqC6@cluster0.9e2ta.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+            //Make sure this is your own unique string
+}
