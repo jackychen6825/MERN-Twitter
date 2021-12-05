57e360290d657d6dd550012cb91330a9c904cd2c
jacky <jc6825@stern.nyu.edu>
2021-12-05 15:38:22 -0500

    complete routes

--- app.js
+++ app.js
@@ -14,3 +14,14 @@ const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
+const bodyParser = require('body-parser');
+
+//middleware
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
